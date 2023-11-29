26f521a653e2151c3952b0707c2af2f6ce981d3b
Davi Reis <dreisss.dev@gmail.com>
2023-11-29 14:41:15 -0300

    fix: fixed "simple-import-sort" plugin error

--- eslint/readme.md
+++ eslint/readme.md
@@ -1,3 +1,21 @@
 # @dreisss/eslint-config
 
 My standard config to eslint config.
+
+## Setup
+
+### Default
+
+- Install eslint and this config:
+
+```
+npm i -D eslint @dreisss/eslint-config
+```
+
+- Add this config to eslint modifying the `.eslintrc.json` file:
+
+```json
+{
+  "extends": ["@dreisss/eslint-config"]
+}
+```
--- eslint/src/index.js
+++ eslint/src/index.js
@@ -1,17 +1,17 @@
 module.exports = {
-  extends: ["standard", "plugin:prettier/recommended"],
-  plugins: ["simple-import-sort"],
+  extends: ['standard', 'plugin:prettier/recommended'],
+  plugins: ['import'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 80,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
+        trailingComma: 'all',
+        arrowParens: 'always',
         semi: false,
       },
     ],
   },
-};
+}
